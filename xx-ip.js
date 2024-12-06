abf12ae9b927439f52772f545f01808500199838
xyhcwu <106462605+xyhcwu@users.noreply.github.com>
2024-12-06 15:39:34 +0800

    Update xx-ip.js

--- xx-ip.js
+++ xx-ip.js
@@ -1,4 +1,4 @@
-const = [
+const ADDAPI = [
 	'https://raw.githubusercontent.com/hubbylei/bestcf/refs/heads/main/bestcf.txt',
 	'https://raw.githubusercontent.com/hubbylei/bestcf/refs/heads/main/bestcf.txt',
 ];
