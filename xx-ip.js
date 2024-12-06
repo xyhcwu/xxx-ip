0f3f3eb439ff1332079610e631e5f25ed0d40d28
xyhcwu <106462605+xyhcwu@users.noreply.github.com>
2024-12-06 14:54:46 +0800

    Update xx-ip.js

--- xx-ip.js
+++ xx-ip.js
@@ -1,5 +1,5 @@
-const txtLinks = {
-  txt1: "https://raw.githubusercontent.com/enricorath/cf-ip-list/refs/heads/main/cloudflare-ip.txt",
-  txt2: "https://dynv6.com/zones/1000.txt"
-};
-console.log(txtLinks);
+fetch('https://raw.githubusercontent.com/enricorath/cf-ip-list/refs/heads/main/cloudflare-ip.txt')
+  .then(response => response.text())
+  .then(data => {
+    console.log(data); // 将.txt内容输出到控制台
+  });
