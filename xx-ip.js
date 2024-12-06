75b78d5d35a287dc9b9d5f04117d0063a892445f
xyhcwu <106462605+xyhcwu@users.noreply.github.com>
2024-12-06 15:21:44 +0800

    Update xx-ip.js

--- xx-ip.js
+++ xx-ip.js
@@ -1,4 +1,4 @@
-let ADDAPI = [
+let addressesapi = [
 	'https://raw.githubusercontent.com/enricorath/cf-ip-list/refs/heads/main/cloudflare-ip.txt',
-	'https://raw.githubusercontent.com/enricorath/cf-ip-list/refs/heads/main/cloudflare-ip.txt',
+	'https://addressesapi.090227.xyz/CloudFlareYes',
 ];
