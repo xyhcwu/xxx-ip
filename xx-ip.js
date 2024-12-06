47f5bbb22af117bca82aaa46467b5e4a62c9ae15
xyhcwu <106462605+xyhcwu@users.noreply.github.com>
2024-12-06 14:51:02 +0800

    Update xx-ip.js

--- xx-ip.js
+++ xx-ip.js
@@ -1,1 +1,5 @@
-https://raw.githubusercontent.com/enricorath/cf-ip-list/refs/heads/main/cloudflare-ip.txt
+const txtLinks = {
+  txt1: "https://raw.githubusercontent.com/enricorath/cf-ip-list/refs/heads/main/cloudflare-ip.txt",
+  txt2: "https://dynv6.com/zones/1000.txt"
+};
+console.log(txtLinks);
