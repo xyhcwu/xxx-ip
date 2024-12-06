9bfcb939791c1a48b66c6603c78d3c5832fcb1da
xyhcwu <106462605+xyhcwu@users.noreply.github.com>
2024-12-06 15:36:15 +0800

    Update xx-ip.js

--- xx-ip.js
+++ xx-ip.js
@@ -1,4 +1,4 @@
-const newAddressesapi = [
+let newAddressesapi = [
 	'https://raw.githubusercontent.com/hubbylei/bestcf/refs/heads/main/bestcf.txt',
 	'https://raw.githubusercontent.com/hubbylei/bestcf/refs/heads/main/bestcf.txt',
 ];
