597b4120f323aba481e576c2b427b5614c01d753
xyhcwu <106462605+xyhcwu@users.noreply.github.com>
2024-12-07 11:11:57 +0800

    Update xx-ip.js

--- xx-ip.js
+++ xx-ip.js
@@ -1,4 +1,4 @@
 const links = [
-    "https://heads/main/ipTop10.html",
+    "https://raw.githubusercontent.com/ZhiXuanWang/cf-speed-dns/refs/heads/main/ipTop10.html",
     "https://github.com/enricorath/cf-ip-list/blob/main/cloudflare-ip.txt",
 ];
